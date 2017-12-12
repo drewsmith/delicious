8cf24ab4e06718685d3b7500e9c6b12606f61439
Drew Smith <andrew.j.smith.jr@gmail.com>
2017-12-11 22:58:53 -0600

    more store logic

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -16,3 +16,32 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Succesfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 }
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', {
+    title: 'Stores',
+    stores
+  });
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render('editStore', {
+    title: `Edit ${store.name}`,
+    store
+  });
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({
+    _id: req.params.id
+  },
+  req.body, {
+      new: true,
+      runValidators: true
+  }).exec();
+
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="${store.slug}">View Store</a>`)
+  res.redirect(`/stores/${store._id}`);
+}
